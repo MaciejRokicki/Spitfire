601d7aa7365f2163a55a3fe321c7052e00aa691b
MaciejRokicki <rokicki.mk@gmail.com>
2020-03-25 14:29:49 +0100

    PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movementSpeed = 2.0f;
+
+    private void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.rb.velocity = new Vector2(0.0f, movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.rb.velocity = new Vector2(-movementSpeed, this.rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.rb.velocity = new Vector2(0.0f, -movementSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            this.rb.velocity = new Vector2(movementSpeed, this.rb.velocity.y);
+        }
+    }
+}
