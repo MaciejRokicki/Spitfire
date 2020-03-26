6d3e7b6f45e47e426d956ddd513280c1bbda1a5f
MaciejRokicki <rokicki.mk@gmail.com>
2020-03-26 13:05:31 +0100

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,10 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public Camera mainCamera;
     public float movementSpeed = 8.0f;
-
+    public bool canShoot = true;
     public GameObject BulletSpawner;
     public GameObject BulletPrefab;
+    public float bulletSpeed = 20.0f;
 
     private void Awake()
     {
@@ -23,28 +24,32 @@ public class PlayerController : MonoBehaviour
     private float time = 0.0f;
     private bool canChangeDirection = true;
     private Vector2 lastMousePositionInDistance;
+
     void Update()
     {
         time += Time.deltaTime;
 
         Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePositionNormalizde = mousePosition.normalized;
         float distance = Vector2.Distance(mousePosition, this.gameObject.transform.position);
         Vector2 destinationPosition = (mousePosition - (Vector2)transform.position).normalized;
 
         if(canChangeDirection && distance > 1.0f)
         {
             rb.velocity = destinationPosition * movementSpeed;
-            lastMousePositionInDistance = mousePosition.normalized;
+            lastMousePositionInDistance = mousePositionNormalizde;
             
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - (Vector3)mousePosition);
+            this.gameObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - (Vector3)mousePosition);
         }
 
-        if(lastMousePositionInDistance == mousePosition.normalized)
+        float changeMousePosition = Vector2.Distance(lastMousePositionInDistance, mousePositionNormalizde);
+
+        if(changeMousePosition <= 0.05f)
             canChangeDirection = false;
         else
             canChangeDirection = true;
             
-        if(time >= 1.0f)
+        if(canShoot && time >= 0.5f)
         {
             Shoot();
             time = 0.0f;
@@ -54,6 +59,6 @@ public class PlayerController : MonoBehaviour
     private void Shoot()
     {
         GameObject bullet = Instantiate(BulletPrefab, BulletSpawner.transform.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody>().velocity = -transform.up * 15.0f;
+        bullet.GetComponent<Rigidbody>().velocity = -transform.up * bulletSpeed;
     }
 }
